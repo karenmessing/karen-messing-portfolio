b72d847193df5de665c2701fa3fb933e759fb169
Brian McAllister <brian@brianmcallister.com>
2013-02-16 20:15:21 -0500

    Add a sample local config.

---
+++ local-config-sample.php
@@ -0,0 +1,13 @@
+<?php /*
+
+  Local configuration.
+  
+*/
+# Environment.
+define('ENV', 'development');
+
+# Database.
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_HOST', '');
