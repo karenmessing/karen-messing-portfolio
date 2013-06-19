1115e88bf84f3eed077ce41bdbe10aa844d50ff9
Brian McAllister <brian@brianmcallister.com>
2013-06-19 19:55:16 -0400

    Add a build profile.

---
+++ app.build.js
@@ -0,0 +1,18 @@
+({
+  appDir: 'content/themes/karenmessing/assets/js',
+  baseUrl: './',
+  mainConfigFile: 'content/themes/karenmessing/assets/js/main.js',
+  
+  optimizeCss: false,
+  dir: 'build/content/themes/karenmessing/assets/js',
+  
+  generateSourceMaps: true,
+  preserveLicenseComments: false,
+  removeCombined: true,
+  
+  modules: [
+    {
+      name: 'main'
+    }
+  ]
+})
