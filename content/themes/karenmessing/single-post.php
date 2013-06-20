742a4355f46c356d6a9f4611b18fd7323a7df033
Brian McAllister <brian@brianmcallister.com>
2013-06-20 19:40:08 -0400

    Add a basic single project page.

---
+++ content/themes/karenmessing/single-post.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<h1 class="project-title"><?php echo $post->post_title; ?></h1>
+
+<div class="project-description">
+  <?php the_field('project_description'); ?>
+</div>
+
+<?php if (get_field('images')): ?>
+  <ul class="images">
+    <?php while (has_sub_field('images')): ?>
+      <li class="image">
+        <?php var_dump(get_sub_field('image')); ?>
+      </li>
+    <?php endwhile; ?>
+  </ul>
+<?php endif; ?>
+
+<?php get_footer(); ?>
