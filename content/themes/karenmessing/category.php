80100cc831241712de38d2ed7a293115d26b5e7c
Jackie Balzer <jackie@behance.com>
2013-07-10 22:28:15 -0400

    Type: Type adjustments including adding a new weight for Brandon

--- content/themes/karenmessing/category.php
+++ content/themes/karenmessing/category.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<h2><?php single_cat_title(); ?></h2>
+<h1><?php single_cat_title(); ?></h1>
 
 <?php foreach ($posts as $project): ?>
   <?php echo $project->post_title; ?>
