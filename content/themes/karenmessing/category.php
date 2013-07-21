2f7684c174c540d60b9b683c7029a884bcd26aee
Jackie Balzer <jackie@behance.com>
2013-07-21 13:13:47 -0400

    Work: Category page styles

--- content/themes/karenmessing/category.php
+++ content/themes/karenmessing/category.php
@@ -5,10 +5,12 @@
 <?php foreach ($posts as $project): ?>
   <div class="images">
     <a href="#" class="navigation-arrow up back-to-top"></a>
-    <p class="description">
-      <a href="<?= get_permalink( $project ); ?>"><?= $project->post_title; ?></a>
-    </p>
-    <img src="<?= get_field('cover_image')['url']; ?>" />
+    <section class="work-item">
+      <p class="description">
+        <a href="<?= get_permalink( $project ); ?>"><?= $project->post_title; ?></a>
+      </p>
+      <img src="<?= get_field('cover_image')['url']; ?>" />
+    </div>
   </div>
 <?php endforeach; ?>
 
